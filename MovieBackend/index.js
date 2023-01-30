ba806017c693f4ef3e43a90669b31df3a258da33
zetsuen <kevnhalim@gmail.com>
2023-01-30 23:44:32 +0700

    feat : removing unused comments

--- MovieBackend/index.js
+++ MovieBackend/index.js
@@ -25,26 +25,3 @@ MongoClient.connect(
         console.log(`Listening on port ${port}...`);
     })
 })
-
-// CURL TESTS
-
-// POST
-// curl -X POST http://localhost:8000/api/v1/reviews/new -H "Content-Type: application/json" -d "{\"movieId\": 12, \"user\": \"beau\", \"review\": \"good\"}"
-// curl -X POST http://localhost:8000/api/v1/reviews/new -H "Content-Type: application/json" -d "{\"movieId\": 12, \"user\": \"quincy\", \"review\": \"bad movie\"}"
-// curl -X POST http://localhost:8000/api/v1/reviews/new -H "Content-Type: application/json" -d "{\"movieId\": 13, \"user\": \"beau\", \"review\": \"awesome\"}"
-
-// curl -X POST http://localhost:8000/api/v1/reviews/new -H "Content-Type: application/json" -d "{\"movieId\": 76600, \"user\": \"Mark\", \"review\": \"Good movie\"}"
-// curl -X POST http://localhost:8000/api/v1/reviews/new -H "Content-Type: application/json" -d "{\"movieId\": 76600, \"user\": \"Robert\", \"review\": \"Not bad, but overall too boring\"}"
-// curl -X POST http://localhost:8000/api/v1/reviews/new -H "Content-Type: application/json" -d "{\"movieId\": 76600, \"user\": \"Gary\", \"review\": \"The duration of the movie is just too long for me personally\"}"
-
-// GET (1 Review)
-// curl -X GET http://localhost:8000/api/v1/reviews/63c5925f4822ba33076efb03
-
-// GET (All Reviews by Movie ID)
-// curl -X GET http://localhost:8000/api/v1/reviews/movie/12
-
-// PUT
-// curl -X PUT http://localhost:8000/api/v1/reviews/63c5925f4822ba33076efb03 -H "Content-Type: application/json" -d "{\"user\": \"beau\", \"review\": \"horrible\"}"
-
-// DELETE
-// curl -X DELETE http://localhost:8000/api/v1/reviews/63c5925f4822ba33076efb03
